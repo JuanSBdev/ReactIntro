40569315d47fc93b3e202177e49d2c7b2d9b4b0f
JuanSBdev <juansb.dev@gmail.com>
2022-05-21 23:09:53 -0300

    avanza

--- introduccion/my-app/src/App.js
+++ introduccion/my-app/src/App.js
@@ -1,13 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/welcome';
-
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Contact from './pages/Contact';
+import About from './pages/About-me';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Welcome message="hola welcome cambiado del prop" name="Master"/>
+        <Router>
+          <div>
+            <ul>
+                <li>
+                <Link to ="/">Casa</Link>
+                </li>
+                <li>
+                  <Link to='/contact'>Contacto</Link>
+                </li>
+                <li>
+                  <Link to='/about'>About ME</Link>
+                </li>
+            </ul>
+          </div>
+          <Routes>
+            <Route path='/contact' element={ <Contact />}/>
+            <Route path='/about' element={ <About />}/>
+
+          </Routes>
+
+        </Router>
       </header>
     </div>
   );
---
+++ introduccion/my-app/src/pages/Contact.js
@@ -0,0 +1,5 @@
+export default function Contact(){
+    return (<div>
+    <h4> Bienvenidos al contacto</h4>
+</div>)
+};
